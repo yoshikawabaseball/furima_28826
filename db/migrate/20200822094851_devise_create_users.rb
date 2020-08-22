46c61d9513b7ada5df146b48dd71e3793f08b6da
yoshikawabaseball <zhixingt9@gmail.com>
2020-08-22 19:55:06 +0900

    ビューファイルの配置

--- db/migrate/20200822094851_devise_create_users.rb
+++ db/migrate/20200822094851_devise_create_users.rb
@@ -10,7 +10,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name,         null: false
       t.string :family_name_kana,   null: false
       t.string :first_name_kana,    null: false
-      t.date   :birth_day,          null: false
+      t.datetime   :birth_day,          null: false
       # t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
