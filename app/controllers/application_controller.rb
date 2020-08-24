328871ca4fba46e257bf382c774ebb16339061d5
yoshikawabaseball <zhixingt9@gmail.com>
2020-08-24 20:03:21 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   before_action :basic_auth
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+  end
 
   private
 
