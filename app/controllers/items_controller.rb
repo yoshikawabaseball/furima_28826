8a2aaad75e678cb5445f9214724a87de8ebcb084
yoshikawabaseball <zhixingt9@gmail.com>
2020-08-26 14:55:36 +0900

    Active Storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
-
-  def move_to_index
+  
+  def create
+    Item.create(item_params)
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:item_image)
+  end
+  
+    def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,10 @@ class Item < ApplicationRecord
   has_many :item_images
   has_many :user_items
   has_many :user, through: :user_items
-  belongs_to_active_hash :category
-  belongs_to_active_hash :item_condition  
-  belongs_to_active_hash :shipping_charge 
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :preparation_day
+  has_one_atatached :image
+  #belongs_to_active_hash :category
+  #belongs_to_active_hash :item_condition  
+  #belongs_to_active_hash :shipping_charge 
+  #belongs_to_active_hash :prefecture
+  #belongs_to_active_hash :preparation_day
 end
