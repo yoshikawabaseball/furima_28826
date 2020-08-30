783ffc3f164e21e67ac1a1b469f6e3fe31a1e82e
yoshikawabaseball <zhixingt9@gmail.com>
2020-08-30 10:48:47 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: :index
+  before_action :set_item, only: :show
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,7 +20,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
@@ -31,5 +31,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
