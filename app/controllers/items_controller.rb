01ad31eafcd825ed31660475e94874051eeb80bd
yoshikawabaseball <zhixingt9@gmail.com>
2020-08-30 14:05:21 +0900

    コントローラーにedit,updateメソッドを定義する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: :index
-  before_action :set_item, only: :show
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,6 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+  
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   private
