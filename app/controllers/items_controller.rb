bfb19c301395aeda2954fa9506eff188dd0f812a
yoshikawabaseball <zhixingt9@gmail.com>
2020-09-09 16:06:13 +0900

    削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,12 +34,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:item_id])
-    if item.destroy
-      redirect_to root_path
-    else
-      render :show
-    end
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
